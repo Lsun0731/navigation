e10afb73c3cc80e5011843645dfbe4867000cefe
舍予 <15650557602@163.com>
2021-09-20 13:23:28 +0800

    minor fix

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    // changeBackground()
+
+
+
+
+
+
+
+
+})
+
+
+// function changeBackground(){
+//     let day=new Date().getDay()
+//     switch (day) {
+//         case 1:
+//             $('.le-customize-body').css('background-image','url(./images/div.jpg)')
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
